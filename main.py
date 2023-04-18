0d0edbc35ec45f7216dacfaa4c9e512975c0cd88
DevoxTec <devoxotieno78@gmail.com>
2023-04-18 14:41:43 +0300

    changes for main.py

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from datetime import *
+
+class Employee:
+
+    def __init__(self, fname, lname, salary):
+        self.fname = fname
+        self.lname = lname
+        self.salary = salary
+
+    def full_name(self):
+        print(f"My full name is {self.fname}{self.lname}")
+
+    def email(self):
+        print(f"{self.fname}{self.lname} @gmail.com")
 
 
-def leaning(course, by_who):
-    course = "git and github"
-    by_who = "Amigoscode"
-
-
-print(datetime)
-leaning()
-print("Hello world!")
+emp_1 = Employee('correy', 'scharfer', 50000)
+emp_1.full_name()
+emp_1.email()
